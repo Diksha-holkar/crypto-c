6c82cfb3b4eaf438cf664fb8be9e471a3f6f9319
Diksha-holkar <dikshaholkar781@gmail.com>
2026-04-06 23:23:09 +0530

    Update flag in checkFlag function

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// Hidden Cipher (player must find this in console)
-console.log("Cipher: dWx3RldJe2Z1YnN3cl9wZHd2aHV9");
-
-// Extra hint in console
-console.log("Hint: First decode, then shift back 😉");
-
 function checkFlag() {
     let userInput = document.getElementById("flagInput").value.trim();
 
-    let correctFlag = "ritCTF{crypto_master}";
+    let correctFlag = "ritCTF{easy_flag}";
 
     if (userInput === correctFlag) {
-        document.getElementById("result").innerHTML = "✅ Correct! You found the flag!";
+        document.getElementById("result").innerHTML = "✅ Correct!";
         document.getElementById("result").style.color = "lightgreen";
     } else {
-        document.getElementById("result").innerHTML = "❌ Wrong flag. Try again!";
+        document.getElementById("result").innerHTML = "❌ Wrong!";
         document.getElementById("result").style.color = "red";
     }
 }
